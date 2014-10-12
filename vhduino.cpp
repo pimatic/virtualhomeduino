27dc45ebe5f71885198f1a28a81ad3b6ea334513
Oliver Schneider <oliverschneider89+sweetpi@gmail.com>
2014-10-12 18:23:23 +0200

    first working version with receiving and sending

--- vhduino.cpp
+++ vhduino.cpp
@@ -14,10 +14,17 @@ extern "C" {
 
 #include "rfcontrol/RFControl.h"
 
+void *interrupt(void *param);
 
-void (*interruptCallback)(void);
-void attachInterrupt(uint8_t, void (*ic)(void), int mode) {
+void (*interruptCallback)(void) = NULL;
+int interrupt_pin = -1;
+pthread_t pth;
+
+void attachInterrupt(uint8_t _interrput_pin, void (*ic)(void), int mode) {
 	interruptCallback = ic;
+	interrupt_pin = _interrput_pin;
+	wiringXISR(interrupt_pin, INT_EDGE_BOTH);
+	pthread_create(&pth, NULL, interrupt, NULL);
 }
 
 unsigned long micros(void) {
@@ -30,14 +37,14 @@ unsigned long micros(void) {
 void realtime() {
 	// Lock memory to ensure no swapping is done.
 	if(mlockall(MCL_FUTURE|MCL_CURRENT)){
-			fprintf(stderr,"WARNING: Failed to lock memory\n");
+		fprintf(stderr,"WARNING: Failed to lock memory\n");
 	}
 
 	// Set our thread to real time priority
 	struct sched_param sp;
 	sp.sched_priority = 30;
 	if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp)){
-			fprintf(stderr,"WARNING: Failed to set thread to real-time priority\n");
+		fprintf(stderr,"WARNING: Failed to set thread to real-time priority\n");
 	}
 }
 
@@ -48,61 +55,150 @@ pthread_mutex_t print_mutex = PTHREAD_MUTEX_INITIALIZER;
 void *interrupt(void *param) {
 	realtime();
 	while(1) {
-		waitForInterrupt(1, 10000);
+		waitForInterrupt(interrupt_pin, 10000);
 		interruptCallback();
 		if(RFControl::hasData()) {
 			unsigned int *timings;
 			unsigned int timings_size;
 			RFControl::getRaw(&timings, &timings_size);
-			pthread_mutex_lock(&print_mutex);
-			for(int i=0; i < timings_size; i++) {
-				printf("%d", timings[i]);
-				printf(" ");
-				if((i+1)%16 == 0) {
-					printf("\n");
-				}
+			unsigned int buckets[8];
+			RFControl::compressTimings(buckets, timings, timings_size);
+			printf("RF receive ");
+			for(unsigned int i=0; i < 8; i++) {
+			 	printf("%d ",buckets[i]);
+			}
+			for(unsigned int i=0; i < timings_size; i++) {
+			 	printf("%d", timings[i]);
 			}
 			printf("\n");
-			printf("\n");
-			pthread_mutex_unlock(&print_mutex);
 			RFControl::continueReceiving();
 		}
 		// int duration = irq_read(1);
 		// printf("d: %d\n", duration);
 	}
+}
 
+char input[256];
+char delimiter[] = " ";
+
+void argument_error() {
+	printf("ERR argument_error\n");
+}
+
+void ping_command() {
+	char *arg;
+	printf("PING");
+	arg = strtok(NULL, delimiter);
+
+	if (arg != NULL) {
+		printf(" %s", arg);
+	}
+	printf("\n");
+}
+
+void rfcontrol_command_receive() {
+	char* arg = strtok(NULL, delimiter);
+	if(arg == NULL) {
+		argument_error();
+		return;
+	}
+	int interrupt_pin = atoi(arg);
+	if(interruptCallback == NULL) {
+		RFControl::startReceiving(interrupt_pin);
+	}
+	printf("ACK\n");
+}
+
+void rfcontrol_command_send() {
+  char* arg = strtok(NULL, delimiter);
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int transmitter_pin = atoi(arg);
+
+  arg = strtok(NULL, delimiter);
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  int repeats = atoi(arg);
+
+  // read pulse lengths
+  unsigned int buckets[8];
+  for(unsigned int i = 0; i < 8; i++) {
+    arg = strtok(NULL, delimiter);
+    if(arg == NULL) {
+      argument_error();
+      return;
+    }
+    buckets[i] = atoi(arg);
+  }
+  //read pulse sequence
+  arg = strtok(NULL, delimiter);
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  unsigned int timings_size = strlen(arg);
+  unsigned int timings[timings_size];
+  for(unsigned int i = 0; i < timings_size; i++) {
+    unsigned int index = arg[i] - '0';
+    timings[i] = buckets[index];
+  }
+  RFControl::sendByTimings(transmitter_pin, timings, timings_size, repeats);
+  printf("ACK\n");
+}
+
+void rfcontrol_command() {
+  char* arg = strtok(NULL, delimiter);
+  if(arg == NULL) {
+    argument_error();
+    return;
+  }
+  if (strcmp(arg, "send") == 0) {
+    rfcontrol_command_send();
+  } else if (strcmp(arg, "receive") == 0) {
+    rfcontrol_command_receive();
+  } else {
+    argument_error();
+  }
 }
 
 
 int main(void) {
-	pthread_t pth;
-
 	wiringXSetup();
-
 	pthread_mutex_init(&print_mutex, NULL);
-
-	pinMode(0, OUTPUT);
-	wiringXISR(1, INT_EDGE_BOTH);
-
-	RFControl::startReceiving(0);
-	pthread_create(&pth, NULL, interrupt, NULL);
-
-	while(1) {
-		char input[256];
-  	 	scanf ("%s",input);
+	printf("ready\n");
+	while(fgets(input, sizeof(input), stdin)) {
+		input[strlen(input)-1] = '\0'; //remove tailing new line
+		//printf("input=\"%s\"", input); 
   	 	pthread_mutex_lock(&print_mutex);
-  	 	printf("input: %s", input);
+  	 	char* command = strtok(input, delimiter);
+  	 	if(strcmp("PING", command) == 0) {
+  	 		ping_command();
+  	 	} else if(strcmp("RF", command) == 0) {
+  	 		rfcontrol_command();
+  	 	} else {
+  	 		printf("ERR unknown_command\n");
+  	 	}
   	 	pthread_mutex_unlock(&print_mutex);
-		usleep(1000);
 	}
 
 	return 0;
 }
 
 
-void pinMode(uint8_t, uint8_t){}
-void digitalWrite(uint8_t, uint8_t){}
-int digitalRead(uint8_t){return 0;}
+void pinMode(uint8_t pin, uint8_t mode){
+	pinMode((int)pin, (int)mode);
+}
+void digitalWrite(uint8_t pin, uint8_t value){
+	digitalWrite((int)pin, (int)value);
+}
+int digitalRead(uint8_t pin){
+	digitalRead((int)pin);
+}
+
 int analogRead(uint8_t){return 0;}
 void analogReference(uint8_t mode){}
 void analogWrite(uint8_t, int){}
