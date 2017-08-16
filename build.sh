8304d207416faf537efc1419fb24a59df5fa4c04
Your Name <you@example.com>
2017-08-16 14:53:34 +0200

    Added build.sh to build from scratch

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export CROSS_BASE="$HOME/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin"
+export PATH=$PATH:$CROSS_BASE
+CROSS_TOOLCHAIN="$CROSS_BASE/arm-linux-gnueabihf-"
+echo $PATH
+sed -i -e "s|CROSS_PREFIX =.*$|CROSS_PREFIX = $CROSS_TOOLCHAIN|" pigpio/Makefile
+cd pigpio
+make
+cd ../virtualhomeduino/Release
+make
