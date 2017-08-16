a287c35cfdc96ca20c868595f96770e5a5e8ace1
Your Name <you@example.com>
2017-08-16 14:58:39 +0200

    Added git checkout for sub modules

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 export CROSS_BASE="$HOME/rpi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/bin"
 export PATH=$PATH:$CROSS_BASE
 CROSS_TOOLCHAIN="$CROSS_BASE/arm-linux-gnueabihf-"
-echo $PATH
+git submodule init
+git submodule update
 sed -i -e "s|CROSS_PREFIX =.*$|CROSS_PREFIX = $CROSS_TOOLCHAIN|" pigpio/Makefile
 cd pigpio
 make
